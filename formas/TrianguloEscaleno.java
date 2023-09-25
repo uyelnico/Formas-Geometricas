daca6e231c1dad2c53b8fb8dab5aea33d8d684e7
Nicolay Chiazzaro Tosquellas <145890121+uyelnico@users.noreply.github.com>
2023-09-25 02:04:27 -0300

    Add files via upload

---
+++ formas/TrianguloEscaleno.java
@@ -0,0 +1,22 @@
+package formas;
+
+import static funcionamiento.colores.*;
+
+import java.util.Scanner;
+
+import funcionamiento.funcion;
+
+public class TrianguloEscaleno {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+        int num = funcion.TamañoTrianguloEscaleno(leer);
+
+       funcion.DibujarTrianguloEscaleno(num, VERDE);
+
+        System.out.println("Un Triángulo Escaleno de tamaño: " + num);
+
+        leer.close();
+    }
+}
