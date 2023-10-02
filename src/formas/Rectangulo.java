0e2f39531d9b1425d3264e17a9e40dd161faf8d3
Nicolay Chiazzaro Tosquellas <145890121+uyelnico@users.noreply.github.com>
2023-10-02 16:19:44 -0300

    Update

--- src/formas/Rectangulo.java
+++ src/formas/Rectangulo.java
@@ -1,5 +1,26 @@
 package formas;
 
-public class Rectangulo {
+import static funcionamiento.colores.*;
+import java.util.Scanner;
+import funcionamiento.funcion;
 
+public class Rectangulo {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+        int alto = funcion.AltoRectangulo(leer);
+        int ancho = funcion.AnchoaRectangulo(leer);
+
+       funcion.DibujarRectangulo(alto, ancho, AZUL);
+
+        System.out.println("Un Rectangulo de alto: " + alto);
+        System.out.println("Un Rectangulo de ancho: " + ancho);
+        
+        leer.close();
+    }
 }
+
+/**
+ * @author Nicolay Chiazzaro
+ *
+ */
