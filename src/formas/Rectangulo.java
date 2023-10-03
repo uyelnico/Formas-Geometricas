62fe877a6ced0256aa8ca644825a0d30bb4860fd
Nicolay Chiazzaro Tosquellas <145890121+uyelnico@users.noreply.github.com>
2023-10-03 11:56:44 -0300

    Update en el Rectangulo (más datos)

--- src/formas/Rectangulo.java
+++ src/formas/Rectangulo.java
@@ -8,13 +8,27 @@ public class Rectangulo {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
 
-        int alto = funcion.AltoRectangulo(leer);
-        int ancho = funcion.AnchoaRectangulo(leer);
-
+        double alto = funcion.AltoRectangulo(leer);
+        double ancho = funcion.AnchoaRectangulo(leer);
+        double area = alto*ancho;
+        double perimetro = 2*(alto + ancho);
+        double diagonal = Math.sqrt(Math.pow(alto, 2) + Math.pow(ancho, 2));
+        
+        String altoDefinitivo = (alto % 1 == 0) ? String.valueOf((int) alto) : String.valueOf(alto);
+        String anchoDefinitivo = (ancho % 1 == 0) ? String.valueOf((int) ancho) : String.valueOf(ancho);
+        String areaDefinitiva = (area % 1 == 0) ? String.valueOf((int) area) : String.valueOf(area);
+        String perimetroDefinitivo = (perimetro % 1 == 0) ? String.valueOf((int) perimetro) : String.valueOf(perimetro);
+        String diagonalDefinitiva = (diagonal % 1 == 0) ? String.valueOf((int) diagonal) : String.valueOf(diagonal);
+        
        funcion.DibujarRectangulo(alto, ancho, AZUL);
 
-        System.out.println("Un Rectangulo de alto: " + alto);
-        System.out.println("Un Rectangulo de ancho: " + ancho);
+        System.out.println("Un Rectangulo de alto: " + altoDefinitivo);
+        System.out.println("Un Rectangulo de ancho: " + anchoDefinitivo);
+        System.out.println("Con un area de: " + areaDefinitiva);
+        System.out.println("Con un perimetro de: " + perimetroDefinitivo);
+        System.out.println("Con una diagonal de: " + diagonalDefinitiva);
+        
+        
         
         leer.close();
     }
