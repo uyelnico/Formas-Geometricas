ceedcfdffffa684bebff0dc784e7a5122eafce8e
Nicolay Chiazzaro Tosquellas <145890121+uyelnico@users.noreply.github.com>
2023-10-04 11:49:47 -0300

    Update Fue agregado una nueva forma geometrica (Triangulo)

--- src/funcionamiento/funcion.java
+++ src/funcionamiento/funcion.java
@@ -35,9 +35,11 @@ public class funcion {
 
     }
     
+    // Dibujar/Imprimir el Cuadrado
+    
     public static void DibujarCuadrado(double tamaño, String color) {
-        for (int x = 1; x <= tamaño; ++x) {
-            for (int y = 1; y <= tamaño; ++y) {
+        for (double x = 1; x <= tamaño; ++x) {
+            for (double y = 1; y <= tamaño; ++y) {
                 System.out.print(color + "  " + color + RESET);
             }
             System.out.println();
@@ -75,7 +77,6 @@ public class funcion {
     }
     
     // Tamaño, Area, Perimetro y Diagonal del Rectangulo
-    
     public static void DatosRectangulo(double tamaño, double area, double perimetro, double diagonal) {
         
         String tamañoDefinitivo = (tamaño % 1 == 0) ? String.valueOf((int) tamaño) : String.valueOf(tamaño);
@@ -90,17 +91,61 @@ public class funcion {
 
     }
  
+    // Dibujar/Imprimir el Rectangulo
     public static void DibujarRectangulo(double alto, double ancho, String color) {
-        for (int x = 1; x <= alto; ++x) {
-            for (int y = 1; y <= ancho; ++y) {
+        for (double x = 1; x <= alto; ++x) {
+            for (double y = 1; y <= ancho; ++y) {
                 System.out.print(color + "  " + color + RESET);
             }
             System.out.println();
         }
     } 
     
-// Triangulo Escaleno
+// Triangulo
+    public static double TamañoTriangulo(Scanner leer) {
+    	double num;
+        do {
+            System.out.print("Ingrese el tamaño (número positivo) para el Triángulo: ");
+            while (!leer.hasNextDouble()) {
+                System.out.print("Ingrese un valor numérico válido: ");
+                leer.next();
+            }
+            num = leer.nextDouble();
+        } while (num <= 0);
+        return num;
+    }
     
+    // Tamaño, Area, Perimetro y Diagonal del Triangulo
+    public static void DatosTriangulo(double tamaño, double area, double perimetro, double diagonal) {
+    	
+        String tamañoDefinitivo = (tamaño % 1 == 0) ? String.valueOf((int) tamaño) : String.valueOf(tamaño);
+        String areaDefinitiva = (area % 1 == 0) ? String.valueOf((int) area) : String.valueOf(area);
+        String perimetroDefinitivo = (perimetro % 1 == 0) ? String.valueOf((int) perimetro) : String.valueOf(perimetro);
+        String diagonalDefinitiva = (diagonal % 1 == 0) ? String.valueOf((int) diagonal) : String.valueOf(diagonal);
+        
+        System.out.println("Un Triangulo Escaleno de tamaño: " + tamañoDefinitivo);
+        System.out.println("Con un area de: " + areaDefinitiva);
+        System.out.println("Con un perimetro de: " + perimetroDefinitivo);
+        System.out.println("Con una diagonal de: " + diagonalDefinitiva);
+    	
+    }
+    
+    // Dibujar/Imprimir el Triangulo
+    public static void DibujarTriangulo(double tamaño, String color) {
+        for (double x = 1; x <= tamaño; ++x) {
+            for (double y = tamaño - x; y > 0; --y) {
+                System.out.print(" ");
+            }
+                for (double a = 1; a <= 2 * x - 1; a++) {
+                    System.out.print(color + " " + color + RESET);
+                }
+            
+            System.out.println();
+        }
+        }
+
+    
+// Triangulo Escaleno
     public static double TamañoTrianguloEscaleno(Scanner leer) {
     	double num;
         do {
@@ -129,6 +174,7 @@ public class funcion {
     	
     }
     
+    // Dibujar/Imprimir el Triangulo Escaleno
     public static void DibujarTrianguloEscaleno(double tamaño, String color) {
         for (int x = 1; x <= tamaño; ++x) {
             for (int y = 1; y <= x; ++y) {
@@ -140,7 +186,6 @@ public class funcion {
     }
 
 // Circulo
-    
     public static double RadioCirculo(Scanner leer) {
     	double radio;
         do {
@@ -154,7 +199,7 @@ public class funcion {
         return radio;
     
 }  
-    
+    // Tamaño, Area, Perimetro y Diagonal del Circulo
     public static void DatosCirculo(double radio, double area, double perimetro, double diagonal) {
     	
         String radioDefinitivo = (radio % 1 == 0) ? String.valueOf((int) radio) : String.valueOf(radio);
@@ -168,7 +213,8 @@ public class funcion {
         System.out.println("Con una diagonal de: " + diagonalDefinitiva);
     	
     }
-    
+  
+    // Dibujar/Imprimir el Circulo
     public static void DibujarCirculo(double radio, String color) {
         for (double y = -radio; y <= radio; y++) {
             for (double x = -radio; x <= radio; x++) {
